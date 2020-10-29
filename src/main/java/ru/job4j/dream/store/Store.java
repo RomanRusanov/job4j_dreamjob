61d936a7382a6a6aede8993179ef295e9a53aa4a
RomanRusanov <roman9628@gmail.com>
2020-10-29 13:04:36 +0300

    Exercise (2. CandidateSerlvet. Create Candidate.[2513#363544]) complete send mentor to code review.

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -25,6 +25,8 @@ public class Store {
 
     private static AtomicInteger POST_ID = new AtomicInteger(4);
 
+    private static AtomicInteger CANDIDATE_ID = new AtomicInteger(4);
+
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job"));
         posts.put(2, new Post(2, "Middle Java Job"));
@@ -46,8 +48,13 @@ public class Store {
         return candidates.values();
     }
 
-    public void save(Post post) {
+    public void savePost(Post post) {
         post.setId(POST_ID.incrementAndGet());
         posts.put(post.getId(), post);
+    }
+
+    public void saveCandidate(Candidate can) {
+        can.setId(CANDIDATE_ID.incrementAndGet());
+        this.candidates.put(can.getId(), can);
     }
 }
