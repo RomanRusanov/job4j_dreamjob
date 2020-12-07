31c36ff31fb5ba96b2c798608c4f421581d08599
RomanRusanov <roman9628@gmail.com>
2020-12-07 19:22:54 +0300

    Exercise (4.1. JSON format.[238554#363519]) complete, send mentor to code review.

--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -1,5 +1,8 @@
 package ru.job4j.dream.servlet;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,11 +14,14 @@ public class GreetingServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/plain");
+        resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
-        String name = req.getParameter("name");
+        String data = req.getParameter("text");
+        System.out.println(data);
+        String json = "{name : " + data + "}";
+        JsonObject jsonObject = new Gson().fromJson(json, JsonObject.class);
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.println("Nice to meet you, " + name);
+        writer.print(jsonObject);
         writer.flush();
     }
 }
